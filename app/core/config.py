02882c2a3a6b88abfe282c925236d0966034a17e
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-04-02 11:22:17 +0900

    feat: OpenAI API로 산책 코스 추천 api 추가

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 from functools import lru_cache
 
 # app 디렉토리 및 .env 파일을 읽어 Settings의 환경변수로 등록
 # .env 파일 내부에 필요 환경변수 작성 필요 ( eg. FIREBASE_AUTH = "cred.json" )
 class Settings(BaseSettings):
-    firebase_auth : str
+    #firebase_auth : str
+    openai_api_key: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='allow')
-
 @lru_cache
 def get_settings():
     return Settings()
@@ -14,5 +15,5 @@ def get_settings():
 settings = get_settings()
 
 # 사용 가능한 모든 환경 변수 출력
-# print(settings.model_dump())
+#print(settings.model_dump())
 
--- app/routers/root.py
+++ app/routers/root.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter, Depends
-from app.dependencies.firebase import get_auth
-
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+# from app.dependencies.firebase import get_auth
+from app.dependencies.openai_dependency import get_openai_client
 router = APIRouter()
 @router.get("/")
 def read_root():
     return {
         "Hello": "World"
     }
+
+@router.get("/walk/recommend")
+def chat_openai(latitude: float, longitude: float, walk_time: int, view: float, difficulty: float, chain = Depends(get_openai_client)):
+    response = chain.invoke({
+        "latitude": latitude,
+        "longitude": longitude,
+        "walk_time": walk_time,
+        "view": view,
+        "difficulty": difficulty
+    })
+    return JSONResponse(content=jsonable_encoder(response))
