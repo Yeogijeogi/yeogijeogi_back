c4f7d0d5aae9b009028879f146e40cd8ec1b4bbb
문혜승 <hyeseungmoon@Hyeseungmoon-MacBookPro-M1.local>
2025-04-06 23:40:38 +0900

    fix: change dockerfile execution location and add PYTHONPATH

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 #
 COPY ./app /code/app
+WORKDIR /code/app
 
+ENV PYTHONPATH="${PYTHONPATH}:/code"
 #
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from typing import Optional
 # app 디렉토리 및 .env 파일을 읽어 Settings의 환경변수로 등록
 # .env 파일 내부에 필요 환경변수 작성 필요 ( eg. FIREBASE_AUTH = "cred.json" )
 class Settings(BaseSettings):
-    firebase_auth: Optional[str] = None
+    firebase_auth: Optional[str] = "firebase_credentials.json"
     mongo_uri: Optional[str] = None
     mongo_database_name: Optional[str] = "test"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='allow')
