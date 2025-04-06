350786d3a9cf88a3beb283aa7ce119baa0c87da4
문혜승 <hyeseungmoon@Hyeseungmoon-MacBookPro-M1.local>
2025-04-06 12:25:23 +0900

    mod: user_id add unique prop

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -1,6 +1,7 @@
-from beanie import Document
+from beanie import Document, Indexed
+from typing import Annotated
 
 # TODO : uuid validator
 
 class Users(Document):
-    user_id:str # firebase uuid
+    user_id:Annotated[str, Indexed(unique=True)] # firebase uuid
