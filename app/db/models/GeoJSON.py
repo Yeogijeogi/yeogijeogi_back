ea56d6cf2a293c6ca4add32642bbf9d98d516dd5
문혜승 <hyeseungmoon@Hyeseungmoon-MacBookPro-M1.local>
2025-04-13 17:35:28 +0900

    fix: GeoJSON validating error

--- app/db/models/GeoJSON.py
+++ app/db/models/GeoJSON.py
@@ -1,16 +1,18 @@
 from typing import List
 from pydantic import BaseModel
 from pydantic import field_validator
+from pydantic import Field
 
 class GeoJSON(BaseModel):
     type:str = "Point"
-    coordinates:List = [0, 0] # long, lat
+    coordinates:List[float] # long, lat
 
     @field_validator('coordinates', mode='after')
     @classmethod
-    def validate_coordinate(cls, coordinates:List):
+    def validate_coordinate(cls, coordinates:List[float]) -> List[float]:
         longitude, latitude = coordinates
         if longitude < -90: raise ValueError("the first element(longitude cannot be smaller than -90")
         if longitude > 90: raise ValueError("the first element(longitude cannot be bigger than 90")
         if latitude < -180: raise ValueError("the second element(longitude cannot be smaller than -180")
         if latitude > 180: raise ValueError("the second element(longitude cannot be bigger than 180")
+        return coordinates
