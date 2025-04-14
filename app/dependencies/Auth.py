aef198bd24324bd81fea084b8f831780419606e4
hyeseungmoon <hyeseungmoon@naver.com>
2025-04-14 23:01:51 +0900

    feat: finish course endpoint

--- app/dependencies/Auth.py
+++ app/dependencies/Auth.py
@@ -1,5 +1,13 @@
-from abc import ABC, abstractmethod
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends, HTTPException
+from app.dependencies.firebase import get_auth
 
-class Auth(ABC):
-    @abstractmethod
-    def verify_token(self, token): pass
+security = HTTPBearer(auto_error=False)
+
+def get_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if not credentials: raise HTTPException(status_code=401, detail="Token is Wrong")
+    return credentials.credentials
+
+def get_uuid(token:str = Depends(get_token),
+             auth = Depends(get_auth)):
+    return auth.verify_token(token)
